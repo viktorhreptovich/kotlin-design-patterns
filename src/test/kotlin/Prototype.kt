6d57a2fcd52cb5c94175070d57883f4ae56cde9b
Viktor Hreptovich <vhreptovich@gmail.com>
2021-01-26 21:31:51 +0300

    Prototype

---
+++ src/test/kotlin/Prototype.kt
@@ -0,0 +1,85 @@
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+abstract class Shape : Cloneable {
+    var id: String? = null
+    var type: String? = null
+    abstract fun draw()
+
+    public override fun clone(): Any {
+        var clone: Any? = null
+        try {
+            clone = super.clone()
+        } catch (e: CloneNotSupportedException) {
+            e.printStackTrace()
+        }
+        return clone!!
+    }
+}
+
+class Rectange : Shape() {
+    override fun draw() {
+        println("Inside Rectangle::draw() method.")
+    }
+
+    init {
+        type = "Rectangle"
+    }
+}
+
+class Square : Shape() {
+    override fun draw() {
+        println("Inside Square::draw() method.")
+    }
+
+    init {
+        type = "Square"
+    }
+}
+
+class Circle : Shape() {
+    override fun draw() {
+        println("Inside Circle::draw() method.")
+    }
+
+    init {
+        type = "Circle"
+    }
+}
+
+object ShapeCache {
+    private val shapeMap = hashMapOf<String?, Shape>()
+
+    fun loadCache() {
+        val circle = Circle()
+        val square = Square()
+        val rectange = Rectange()
+
+        shapeMap.put("1", circle)
+        shapeMap.put("2", square)
+        shapeMap.put("3", rectange)
+    }
+
+    fun getShape(shapeId: String): Shape {
+        val cachedShape: Shape? = shapeMap.get(shapeId)
+        return cachedShape?.clone() as Shape
+    }
+}
+
+class PrototypeTest {
+    @Test
+    fun prototypeTest() {
+        ShapeCache.loadCache()
+        val clonedShape1 = ShapeCache.getShape("1")
+        val clonedShape2 = ShapeCache.getShape("2")
+        val clonedShape3 = ShapeCache.getShape("3")
+
+        clonedShape1.draw()
+        clonedShape2.draw()
+        clonedShape3.draw()
+
+        Assertions.assertThat(clonedShape1.type).isEqualTo("Circle")
+        Assertions.assertThat(clonedShape2.type).isEqualTo("Square")
+        Assertions.assertThat(clonedShape3.type).isEqualTo("Rectangle")
+    }
+}
